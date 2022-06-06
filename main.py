99f7c78ed4fcd744451f6e1f17fa4097d238292b
Marv_K254 <marvinkorir.mk@gmail.com>
2022-06-06 16:39:16 +0300

    learnt data types in python

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-unit = "hours"
+# unit = "hours"
 calc_to_unit = 24
 
 
-def days_to_units(days):
-    print(f"{days} days is equivalent to {days * calc_to_unit} {unit}")
+def days_to_units(days, unit):
+    if unit == "hours":
+        print(f"{days} days is equivalent to {days * 24} {unit}")
+    elif unit == "minutes":
+        print(f"{days} days is equivalent to {days * 24 * 60} {unit}")
+    else:
+        return "unit not valid "
 
 
 def validate_and_execute():
     try:
-        user_input = int(num_of_day)
+        user_input = int(days_and_units_dictionary["days"])
 
         # do conversion for positive numbers only
         if user_input > 0:
-            return days_to_units(user_input)
+            return days_to_units(user_input, days_and_units_dictionary["unit"])
         elif user_input == 0 or user_input > 0:
             print("Enter more than zero")
         else:
@@ -24,7 +29,6 @@ def validate_and_execute():
 num_of_days = ""
 while num_of_days != "except":
     num_of_days = input("Enter the number days to convert\n")
-    print(type(num_of_days.split(",")))
-    print(num_of_days.split(","))
-    for num_of_day in num_of_days.split(","):
-        validate_and_execute()
+    list_of_days = num_of_days.split(":")
+    days_and_units_dictionary = {"days": list_of_days[0], "unit": list_of_days[1]}
+    validate_and_execute()
